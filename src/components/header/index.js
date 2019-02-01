deddd19965138d7a2c2facdfc780e23cc5caf426
Marc <marc.berli@windowslive.com>
2019-02-01 15:24:52 +0100

    Add possibility to filter event locations

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
-const Header = () => (
-  <div className="header">
-    <div className="search">
-      <input type="text" placeholder="Wo?" />
-      <button></button>
-    </div>
-  </div>
-)
+const Header = ({ setFilter }) => {
+  const [searchValue, setSearchValue] = useState('')
 
+  const onSubmit = e => {
+    e.preventDefault();
+    setFilter({ location: searchValue })
+  }
+
+  return (
+    <div className="header">
+      <form className="search" onSubmit={onSubmit}>
+        <input type="text" placeholder="Wo?" onChange={e => setSearchValue(e.target.value)}/>
+        <button type="submit"></button>
+      </form>
+    </div>
+  )
+}
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StaticQuery, graphql } from 'gatsby';
 import Header from 'components/header';
 import DateNav from 'components/date-nav';
@@ -7,52 +7,60 @@ import EventCard from 'components/event-card';
 import './index.scss';
 import './reset.scss'
 
-const IndexPage = () => (
-  <div id="root">
-    <Header />
-    <div className="page-content">
-      <StaticQuery
-        query={graphql`{
-          craft {
-            entries(section:[events], orderBy: "date") {
-              ...on Craft_Events {
-                title,
-                location,
-                date,
-                image {
-                  url
-                },
-                price {
-                  ...on Craft_PricePrice {
-                    prices,
-                    description
+
+const IndexPage = () => {
+  const [ filter, setFilter ] = useState({})
+
+  const eventMatchesFilter = event =>
+    Object.keys(filter).every(key => event[key].toLowerCase().indexOf(filter[key].toLowerCase()) >= 0)
+
+  return (
+    <div id="root">
+      <Header setFilter={setFilter}/>
+      <div className="page-content">
+        <StaticQuery
+          query={graphql`{
+            craft {
+              entries(section:[events], orderBy: "date") {
+                ...on Craft_Events {
+                  title,
+                  location,
+                  date,
+                  image {
+                    url
+                  },
+                  price {
+                    ...on Craft_PricePrice {
+                      prices,
+                      description
+                    }
                   }
                 }
               }
             }
-          }
-        }`}
-        render={({ craft }) => {
-          const eventEntries = craft.entries
-          return (
-            <div className="events-container">
-              {eventEntries.map(eventEntry => (
-                <EventCard
-                  key={eventEntry.id}
-                  eventName={eventEntry.title}
-                  date={eventEntry.date}
-                  location={eventEntry.location}
-                  price={eventEntry.price[0] || {}}
-                  imageUrl={eventEntry.image[0] ? eventEntry.image[0].url : ''}
-                />
-              ))}
-            </div>
-            );
-          }}
-      />
+          }`}
+          render={({ craft }) => {
+            const eventEntries = craft.entries
+            return (
+              <div className="events-container">
+                {eventEntries.map(eventEntry =>
+                  eventMatchesFilter(eventEntry) && (
+                  <EventCard
+                    key={eventEntry.id}
+                    eventName={eventEntry.title}
+                    date={eventEntry.date}
+                    location={eventEntry.location}
+                    price={eventEntry.price[0] || {}}
+                    imageUrl={eventEntry.image[0] ? eventEntry.image[0].url : ''}
+                  />
+                ))}
+              </div>
+              );
+            }}
+        />
+      </div>
+      <DateNav setFilter={setFilter}/>
     </div>
-    <DateNav />
-  </div>
-);
-
+  );
+}
 export default IndexPage;
