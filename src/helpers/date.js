de6c122e273906123c17b50b82654daed1e0a2e1
Marc <marc.berli@windowslive.com>
2019-02-01 17:45:28 +0100

    Remove now unused helper method

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,4 @@
 import moment from 'moment';
-import { pad } from 'helpers/string';
 
 export const parseDateFromCraftTimestamp = timestamp => {
     return new moment(timestamp * 1000);
--- src/helpers/string.js
+++
@@ -1,3 +0,0 @@
-export const pad = num => {
-  return num < 10 ? "0" + num : num
-}
