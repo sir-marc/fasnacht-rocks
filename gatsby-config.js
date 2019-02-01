093066a8a99b4024b723f8596ac4ceef0891ac96
Marc <marc.berli@windowslive.com>
2019-02-01 17:54:30 +0100

    Update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2E76A4`,
+        theme_color: `#2E76A4`,
         display: `minimal-ui`,
         icon: `src/images/party-emoji.png`, // This path is relative to the root of the site.
       },
