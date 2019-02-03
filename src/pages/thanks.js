57cbc996dc7d7b7b9e71214c39d13c460c5be794
Marc <marc.berli@windowslive.com>
2019-02-03 20:51:39 +0100

    Add styling for thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Header from 'components/layout/header'
 import Content from 'components/layout/content'
 
+import './thanks.scss'
+
 const Thanks = () => (
-  <>
+  <div className="thanks-page">
     <Header>
       <h1>Vielen Dank!</h1>
     </Header>
     <Content>
-      <a href="/">Zurück zur Übersicht</a>
+      <a className="back-to-overview" href="/">Zurück zur Übersicht</a>
     </Content>
-  </>
+  </div>
 )
 
 export default Thanks
