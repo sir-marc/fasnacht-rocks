3f59742ae77d88ca169b35aeb4366e8105939fe3
Marc <marc.berli@windowslive.com>
2019-02-03 22:02:06 +0100

    Fix LinkButton a11y warning and variable naming

--- src/components/button/link-button.js
+++ src/components/button/link-button.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './index.scss'
 
-const Button = props => <a className="button" { ...props }/>
+const Button = ({ children, ...props}) => (
+  <a className="button" { ...props }>{children}</a>
+)
 
 export default Button
